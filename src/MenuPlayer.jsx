a97813e0c7c4c18cc8db32850ea2a3a2dad7aa8b
João Victor Monção <joao.victor.moncaomatos@gmail.com>
2025-11-10 20:38:24 -0300

    react + bootstrap

---
+++ src/MenuPlayer.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './MenuPlayer.css'
+
+function MenuPlayer() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default MenuPlayer
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importando Bootstrap CSS
 import './index.css'
-import App from './App.jsx'
+import MenuPlayer from './MenuPlayer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MenuPlayer/>
   </StrictMode>,
 )
