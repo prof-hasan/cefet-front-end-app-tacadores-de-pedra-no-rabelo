65eeda57805c04680bcaf6201e84b2db4c05c64b
João Victor Monção <joao.victor.moncaomatos@gmail.com>
2025-11-11 19:17:21 +0000

    personagem e imagem

--- jogo/jogo.js
+++ jogo/jogo.js
@@ -37,3 +37,31 @@ naveEl.addEventListener("keypress", (e) => {
     naveEl.style.left = x;
     naveEl.style.top = y;
 });
+
+ // tela do jogo
+    const container = document.createElement('div');
+    container.style.position = 'fixed';
+    container.style.top = '0';
+    container.style.left = '0';
+    container.style.width = '100vw';
+    container.style.height = '100vh';
+    container.style.background = '#222';
+    container.style.display = 'flex';
+    container.style.justifyContent = 'center';
+    container.style.alignItems = 'center';
+    container.id = 'tela';
+
+    // personagem
+    const personagem = document.createElement('div');
+    personagem.id = 'personagem';
+    personagem.style.width = '50px';
+    personagem.style.height = '50px';
+    personagem.style.background = 'rgba(83, 206, 34, 1)';
+    personagem.style.borderRadius = '10px';
+    personagem.style.position = 'absolute';
+    personagem.style.display = 'flex';
+    personagem.style.justifyContent = 'center';
+    personagem.style.alignItems = 'center';
+    personagem.style.fontFamily = 'Arial, sans-serif';
+    personagem.style.fontSize = '20px';
+    personagem.style.color = '#fff';
